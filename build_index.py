07deeac94770e612997df273c0daddf9e7bf0051
Matvey Nikitichev <matvey.nikitichev@protonmail.com>
2024-08-07 16:51:48 +0100

    add folders and other file

--- build_index.py
+++ build_index.py
@@ -7,7 +7,7 @@ with open("index.md") as f:
     data = f.read()
 
 html_data = markdown.markdown(data)
-formatted_page = template.replace("%CONTENT%", html_data)
+formatted_page = template.replace("../stylesheets/style.css", "stylesheets/style.css").replace("%CONTENT%", html_data)
 
-with open(f"index.html", "w") as g:
+with open("index.html", "w") as g:
     g.write(formatted_page)
